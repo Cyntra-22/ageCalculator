5754863254f2e55f4bff4319f5925b6e9827f745
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2024-04-30 15:10:01 +0700

    created the main container

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,41 @@
 	
 </script>
 
-<main>
-</main>
 
 <style>
-	
+	:root{
+		--font-color: hsl(259, 100%, 65%);
+		--font: 'Poppins', sans-serif;
+		--bgColor:  hsl(0, 0%, 86%);
+	}
+
+	:global(body){
+		padding: 0;
+		margin: 0;
+		font-family: var(--font); 
+		background-color: var(--bgColor);  	 
+	}
+
+
+	.main-container{
+		
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%,-50%);
+		background-color: hsl(0, 0%, 100%);
+		padding: 20px;
+		width: 70%;
+		box-shadow: 0 6px 6px rgba(0, 0, 0, 0.1);
+		border-radius: 10px 10px 100px 10px;
+		height: 300px;
+		
+	}
+
 </style>
+
+<main>
+	<div class="main-container">
+
+	</div>
+</main>
