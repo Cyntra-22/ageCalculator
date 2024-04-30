d3080e28b79710eb0c39394617a52104064a3470
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2024-04-30 16:16:14 +0700

    Added Date component and modified the code for style

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	
+	import Date from "./Date.svelte";
 </script>
 
 
@@ -8,6 +8,7 @@
 		--font-color: hsl(259, 100%, 65%);
 		--font: 'Poppins', sans-serif;
 		--bgColor:  hsl(0, 0%, 86%);
+		--lineColor: hsl(0, 1%, 44%);
 	}
 
 	:global(body){
@@ -26,11 +27,51 @@
 		transform: translate(-50%,-50%);
 		background-color: hsl(0, 0%, 100%);
 		padding: 20px;
-		width: 70%;
+		width: 30%;
 		box-shadow: 0 6px 6px rgba(0, 0, 0, 0.1);
 		border-radius: 10px 10px 100px 10px;
 		height: 300px;
+					
+	}
+	.date-conponent{
+		width: 70%;
+		padding-left: 25px;
+	}
+
+	.line-container{
+		display: flex;
+		align-items: center;
+		width: 90%;
+	}
+
+	.line{
+		flex: 1;
+		border-top: 1px solid hsl(0, 0%, 86%);
 		
+	}
+
+	.img-style{
+		background-color: var(--font-color);
+		border-radius: 50%;
+		cursor: pointer;
+		width: 60px;
+		height: 60px;
+	}
+
+	img{
+		width: 60px;
+		height: 25px;
+		padding-top: 17px;
+	}
+
+	.img-style:hover{
+		background-color: black;
+	}
+
+	.center-container{
+		display: flex;
+		flex-direction: column;
+		align-items: center; 
 	}
 
 </style>
@@ -38,5 +79,16 @@
 <main>
 	<div class="main-container">
 
+			<div class="date-conponent">
+					<Date  />
+			</div>
+			<div class="center-container">
+				<div class="line-container">
+					<div class="line"></div>
+					<div class="img-style">
+						<img src="/icon-arrow.svg" alt="logo arrow" />
+					</div>
+				</div>
+			</div>		
 	</div>
 </main>
---
+++ src/Date.svelte
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<style>
+    .date-container{
+        display: flex;
+        justify-content: space-between;
+    }
+
+    input{
+        margin-top: 7px;
+        width: 80px;
+    }
+</style>
+
+<div class="date-container">
+    <div>
+        <label for="dayInput">DAY</label>
+        <input type="number" placeholder = "DD"/>
+    </div>
+    <div>
+        <label for="dayInput">MONTH</label>
+        <input type="number" placeholder = "MM"/>
+    </div>
+    <div>
+        <label for="dayInput">YEAR</label>
+        <input type="number" placeholder = "YY"/>
+    </div>
+</div>
